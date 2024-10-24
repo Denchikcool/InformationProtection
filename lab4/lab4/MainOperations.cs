50c3bcee8969c40365a2899c0f5acabb1b1b3892
Denchikcool <kuzmenok.den@yandex.ru>
2024-10-24 14:34:48 +0700

    Ментальный покер, протокол для обеспечения честной игры

---
+++ lab4/lab4/MainOperations.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class MainOperations
+    {
+        public static long FastPow(long a, long x, long p)
+        {
+            long result = 1;
+            List<long> temp = new List<long> { a % (long)p };
+            long t = (long)Math.Floor(Math.Log(x, 2));
+
+            List<long> binaryExponent = ToBinary(x);
+
+            for (int i = 1; i <= t; i++)
+            {
+                temp.Add((temp[i - 1] * temp[i - 1]) % (long)p);
+            }
+
+            for (int i = 0; i <= t; i++)
+            {
+                if (binaryExponent[i] != 0)
+                {
+                    result = (result * temp[i]) % (long)p;
+                }
+            }
+            return result;
+        }
+
+        private static List<long> ToBinary(long x)
+        {
+            List<long> result = new List<long>();
+            while (x != 0)
+            {
+                result.Add(x & 1);
+                x = x >> 1;
+            }
+            return result;
+        }
+
+        public static long GenerateModule(long left, long right, Random rnd)
+        {
+            //Random rnd = new Random();
+            long p = 0;
+            long range = right - left + 1;
+            while (true)
+            {
+                //p = rnd.Next((int)left, (int)right);
+                p = (long)(rnd.NextDouble() * range) + left;
+                if (IsPrime(p))
+                {
+                    return p;
+                }
+            }
+        }
+
+        //Ферма
+        public static bool IsPrime(long number)
+        {
+            Random rnd = new Random();
+            if (number <= 1) return false;
+            else if (number == 2) return true;
+            for (long i = 0; i < 100; i++)
+            {
+                long a = (long)rnd.Next(2, (int)number - 1);
+                if (FastPow(a, number - 1, number) != 1 || Gcd(number, a) != 1) return false;
+            }
+            return true;
+        }
+
+        public static long Gcd(long a, long b)
+        {
+            while (b != 0)
+            {
+                long r = a % b;
+                a = b;
+                b = r;
+            }
+            return a;
+        }
+
+        public static (long, long, long) EvklidSolve(long a, long b)
+        {
+            (long, long, long) U = (a, 1, 0);
+            (long, long, long) V = (b, 0, 1);
+
+            while (V.Item1 != 0)
+            {
+                long q = U.Item1 / V.Item1;
+                (long, long, long) T = (U.Item1 % V.Item1, U.Item2 - q * V.Item2, U.Item3 - q * V.Item3);
+                U = V;
+                V = T;
+            }
+
+            return U;
+        }
+
+        public static long GenerateCoprime(long p, long start)
+        {
+            long result = 0;
+            for (long i = start; i < p; i++)
+            {
+                if (Gcd(p, i) == 1)
+                {
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ lab4/lab4/lab4.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class lab4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("♠ ♣ ♥ ♦");
+            MentalPoker(4);
+        }
+
+        static void MentalPoker(int playersNum)
+        {
+            long p, q, counter = 3;
+            List<long> C = new List<long>();
+            List<long> D = new List<long>();
+            Random rnd = new Random();
+
+            while (true)
+            {
+                q = MainOperations.GenerateModule(1000000, 1000000000, rnd);
+                p = 2 * q + 1;
+                if (MainOperations.IsPrime(p))
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"q = {q}");
+            Console.WriteLine($"p = {p}");
+
+            for (int i = 0; i < playersNum; i++)
+            {
+                long C_temp = MainOperations.GenerateCoprime(p - 1, counter);
+                long D_temp = MainOperations.EvklidSolve(C_temp, p - 1).Item2;
+                if (D_temp < 0)
+                {
+                    D_temp += (p - 1);
+                }
+                Console.WriteLine($"C_temp * D_temp mod (p-1) = {MainOperations.FastPow(C_temp * D_temp, 1, p - 1)}");
+                C.Add(C_temp);
+                D.Add(D_temp);
+                counter += 5;
+            }
+            
+            Console.WriteLine($"C = {string.Join(", ", C)}");
+            Console.WriteLine($"D = {string.Join(", ", D)}");
+
+            Dictionary<long, string> originDeck = GenDeck();
+            Console.WriteLine($"Deck: {string.Join(", ", originDeck.Values)}");
+
+            List<long> deckKeys = originDeck.Keys.ToList();
+            Console.WriteLine($"Deck Keys: {string.Join(", ", deckKeys)}");
+            Random random = new Random();
+            for (int i = 0; i < playersNum; i++)
+            {
+                deckKeys = deckKeys.Select(K => MainOperations.FastPow(K, C[i], p)).ToList();
+                Shuffle(deckKeys, random);
+                Console.WriteLine($"Shuffle {i + 1}: ");
+                Console.WriteLine($"{string.Join(", ", deckKeys)}");
+            }
+
+            List<List<long>> hands = new List<List<long>>();
+            for (int i = 0; i < playersNum; i++)
+            {
+                hands.Add(new List<long>());
+                for (int j = 0; j < 2; j++)
+                {
+                    long card = deckKeys[j];
+                    deckKeys.Remove(card);
+                    hands[i].Add(card);
+                }
+            }
+
+            Console.WriteLine($"Hands: {string.Join(", ", hands.Select(hand => $"[{string.Join(", ", hand)}]"))}");
+
+            List<long> table = deckKeys.Take(5).ToList();
+            Console.WriteLine($"Table: {string.Join(", ", table)}");
+
+            for (int i = 0; i < playersNum; i++)
+            {
+                table = table.Select(card => MainOperations.FastPow(card, D[i], p)).ToList();
+            }
+            Console.WriteLine($"Table decoded: {string.Join(", ", table)}");
+
+            Dictionary<long, string> tableCards = table.ToDictionary(key => key, key => originDeck[key]);
+            Console.WriteLine($"\nCards on the table: {string.Join(" ", tableCards.Values.Select(card => GetColoredCard(card)))}");
+
+            for (int i = 0; i < playersNum; i++)
+            {
+                for (int j = 0; j < playersNum; j++)
+                {
+                    if (i != j)
+                    {
+                        for (int v = 0; v < hands[i].Count; v++)
+                        {
+                            hands[i][v] = MainOperations.FastPow(hands[i][v], D[j], p);
+                        }
+                    }
+                }
+                for (int v = 0; v < hands[i].Count; v++)
+                {
+                    hands[i][v] = MainOperations.FastPow(hands[i][v], D[i], p);
+                }
+            }
+
+            List<Dictionary<long, string>> processedHands = hands.Select(hand => hand.ToDictionary(kvp => kvp, kvp => originDeck.ContainsKey(kvp) ? originDeck[kvp] : "Invalid card")).ToList();
+
+            for (int i = 0; i < playersNum; i++)
+            {
+                Console.Write($"Player {i + 1} has cards: ");
+                foreach (string card in processedHands[i].Values)
+                {
+                    Console.Write(card + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static Dictionary<long, string> GenDeck()
+        {
+            string[] suits = { "♠", "♣", "♥", "♦" };
+            string[] faces = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+            Dictionary<long, string> deck = new Dictionary<long, string>();
+            long index = 2;
+            foreach (string suit in suits)
+            {
+                foreach (string face in faces)
+                {
+                    deck[index] = $"{suit}{face}";
+                    index++;
+                }
+            }
+            return deck;
+        }
+
+        private static string GetColoredCard(string card)
+        {
+            return $"{card}";
+        }
+
+        private static void Shuffle<T>(List<T> list, Random rng)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+    }
+}
