8511f08878a0b3815272a328b8b3fe68dfbe5bfe
Denchikcool <kuzmenok.den@yandex.ru>
2024-11-05 11:56:44 +0700

    Добавлена проверка раскрски графа на стороне Боба

--- RGR/RGR/Program.cs
+++ RGR/RGR/Program.cs
@@ -53,6 +53,7 @@ namespace RGR
                 for (int i = 0; i < numVertices; i++)
                 {
                     ulong r = MainOperations.GenerateExponent(1000000, 1000000000, random);
+                    Console.WriteLine($"r = {r}");
                     vertexesWithR.Add(r);
                 }
 
@@ -80,11 +81,12 @@ namespace RGR
                     {
                         {"N", rsa.N },
                         {"D", rsa.D },
-                        {"C", rsa.C }
+                        {"C", rsa.C },
+                        {"phi", rsa.phi }
                     };
 
                     informationVertexes.Add(vertexInfo);
-                    Console.WriteLine($"R = {vertexesWithR[i]}, D = {informationVertexes[i]["D"]}, N = {informationVertexes[i]["N"]}");
+                    Console.WriteLine($"R = {vertexesWithR[i]}, D = {informationVertexes[i]["D"]}, N = {informationVertexes[i]["N"]}, C = {informationVertexes[i]["C"]}, phi = {informationVertexes[i]["phi"]}");
                     zVertex.Add(MainOperations.FastPow1(vertexesWithR[i], informationVertexes[i]["D"], informationVertexes[i]["N"]));
                 }
 
@@ -94,12 +96,56 @@ namespace RGR
                     Console.WriteLine($"Вершина {i + 1}: R = {vertexesWithR[i]}, Цвет = {vertexColors[i]}, Двоичный код = {colorToBinary[vertexColors[i]]}");
                     Console.WriteLine($"N = {informationVertexes[i]["N"]}, C = {informationVertexes[i]["C"]}, D = {informationVertexes[i]["D"]}");
                     Console.WriteLine($"Z = {zVertex[i]}");
+                    Console.WriteLine($"C * D % N = {MainOperations.FastPow1(informationVertexes[i]["C"] * informationVertexes[i]["D"], 1, informationVertexes[i]["phi"])}");
                 }
+
+                BobChecks(edges, vertexesWithR, informationVertexes, zVertex);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Ошибка: {ex.Message}");
             }
+        }
+
+        static void BobChecks(List<Edge> edges, List<ulong> vertexesWithR, List<Dictionary<string, BigInteger>> informationVertexes, List<BigInteger> zVertex)
+        {
+            string lastTwoBits_r1 = "", lastTwoBits_r2 = "";
+            Console.WriteLine("Начинаю проверку раскраски графа Алисой.");
+            for (int i = 0; i < edges.Count; i++)
+            {
+                int source = edges[i].Source;
+                int destination = edges[i].Destination;
+
+                BigInteger nSource = informationVertexes[source]["N"];
+                BigInteger cSource = informationVertexes[source]["C"];
+
+                BigInteger nDestination = informationVertexes[destination]["N"];
+                BigInteger cDestination = informationVertexes[destination]["C"];
+
+                BigInteger _r1 = MainOperations.FastPow1(zVertex[source], cSource, nSource);
+                BigInteger _r2 = MainOperations.FastPow1(zVertex[destination], cDestination, nDestination);
+
+                string _r1String = _r1.ToString();
+                string _r2String = _r2.ToString();
+
+                // Проверка на равенство последних двух битов
+                if (_r1String.Length >= 2 && _r2String.Length >= 2)
+                {
+                    lastTwoBits_r1 = _r1String.Substring(_r1String.Length - 2);
+                    lastTwoBits_r2 = _r2String.Substring(_r2String.Length - 2);
+
+                    if (lastTwoBits_r1 == lastTwoBits_r2)
+                    {
+                        Console.WriteLine($"Ошибка: Вершины {source} и {destination} имеют одинаковые последние два бита в результатах возведения в степень.");
+                        return;
+                    }
+                }
+                Console.WriteLine($"Вершина {source + 1}: Z = {zVertex[source]}, C = {cSource}, N = {nSource}, результат: {_r1}, последние два бита: {lastTwoBits_r1}");
+                Console.WriteLine($"Вершина {destination + 1}: Z = {zVertex[destination]}, C = {cDestination}, N = {nDestination}, результат: {_r2}, последние два бита: {lastTwoBits_r2}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Граф правильно раскрашен.");
         }
     }
 }
