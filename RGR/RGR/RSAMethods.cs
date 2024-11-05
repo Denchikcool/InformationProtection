3836e95d2de32ef24db2a70278647d7125d39365
Denchikcool <kuzmenok.den@yandex.ru>
2024-11-05 11:57:18 +0700

    Изменены типы данных у которых параметров для корректной работы

--- RGR/RGR/RSAMethods.cs
+++ RGR/RGR/RSAMethods.cs
@@ -10,8 +10,9 @@ namespace RGR
     class RSAMethods
     {
         internal BigInteger N;
-        internal ulong C;
+        internal BigInteger C;
         internal ulong D;
+        internal BigInteger phi;
         private static Random rnd = new Random();
         public RSAMethods()
         {
@@ -20,29 +21,15 @@ namespace RGR
             ulong Q = MainOperations.GenerateModule(1000000, 1000000000, rnd);
 
             N = P * Q;
-            ulong phi = (P - 1) * (Q - 1);
-            D = GenerateCoprime(phi);
+            phi = (P - 1) * (Q - 1);
+            D = MainOperations.GenerateCoprime(phi);
 
-            C = MainOperations.EvklidSolve(D, phi).Item2;
+            C = MainOperations.EvklidSolve1(D, phi).Item2;
 
             if(C < 0)
             {
                 C += phi;
             }
-        }
-
-        private static ulong GenerateCoprime(ulong p)
-        {
-            ulong result = 0;
-            for (ulong i = 2; i < p; i++)
-            {
-                if (MainOperations.Gcd(p, i) == 1)
-                {
-                    result = i;
-                    break;
-                }
-            }
-            return result;
         }
     }
 }
